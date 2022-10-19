44841ab33b05fb9efefc9a8264d884f960cf6ecf
Hemanth-4010 <115634209+Hemanth-4010@users.noreply.github.com>
2022-10-19 16:28:06 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,11 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+struct Stats compute_statistics(const float* numberset, int setlength)
+{
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
-}
 
 if (setlength == 0 || numberset == 0)
 {
